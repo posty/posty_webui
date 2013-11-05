067f5cc4148eb07768a8b623d6942b9046a2de86
lweis <lukas@schreibt-dir.de>
2013-11-05 12:56:23 +0100

    Revert "Update conf.js"
    This reverts commit c2b26ebc615dd6e84facfd93b4a0c14205267f96.

--- conf.js
+++ conf.js
@@ -2,5 +2,5 @@
 * configuration
 */
 
-var serverUrl = 'URL to posty_API';
-var serverAuthKey = 'YOUR API KEY';
+var serverUrl = 'http://posty-api-development.herokuapp.com/api/v1';
+var serverAuthKey = '123456';
